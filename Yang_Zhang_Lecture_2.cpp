50b97354590692482366c89747bd692022c5b92f
yangzhang31 <yangz15@nycstudents.net>
2024-09-17 15:40:04 -0400

    Hello

--- Yang_Zhang_Lecture_2.cpp
+++ Yang_Zhang_Lecture_2.cpp
@@ -16,34 +16,34 @@ int main() {
     string stringS = "Speed";
 
     //Print Variable Values
-    cout<< "int:" << intS << "\n";
-    cout<< "float:" << floatS << "\n";
-    cout<< "double:" << doubleS << "\n";
-    cout<< "char:" << charS << "\n";
-    cout<< "bool:" << boolS << "\n";
-    cout<< "string:" << stringS << "\n";
+    cout<< "int: " << intS << "\n";
+    cout<< "float: " << floatS << "\n";
+    cout<< "double: " << doubleS << "\n";
+    cout<< "char: " << charS << "\n";
+    cout<< "bool: " << boolS << "\n";
+    cout<< "string: " << stringS << "\n";
+    cout<< " " << "\n";
 
-    //Arthimatic with Variables
+    //Code from Slide 37 
     int inches;
     inches = 100;
 
     cout << inches << " Inch(es) = ";
     cout << inches / 12 << " feet (foot) and ";
     cout << inches % 12 << " inch(es) " << "\n";
+    cout<< " " << "\n";
 
     //Comparing Variables 
     char A = 'A';
     char B = 'B';
-    
-    if (A > B) then 
-        cout << " A > B " << "\n";
-    if (A < B) then 
-        cout << " A < B " << "\n";
 
-    //Expressions with Variables
-    cout << " 2 + 3.5   " << 2 + 3.5 << "\n";
-    cout << " 6 / 4 + 3.9 " << 6 / 4 + 3.9 << "\n";
-    cout << " 5.4 * 2 - (13.6 + 18 / 2) " << 5.4 * 2 - (13.6 + 18 / 2) << "\n";
+    cout << "A < B  = " << (A < B) << endl;
+    cout<< " " << "\n";
+
+    //Arthimatic with Variables
+    cout << " 2 + 3.5 = " << 2 + 3.5 << "\n";
+    cout << " 6 / 4 + 3.9 = " << 6 / 4 + 3.9 << "\n";
+    cout << " 5.4 * 2 - (13.6 + 18 / 2) = " << 5.4 * 2 - (13.6 + 18 / 2) << "\n";
 
     return 0;
 }
